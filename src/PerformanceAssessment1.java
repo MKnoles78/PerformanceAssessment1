6d0c9e66d96caebad95292ab94ba0ae02ea9f388
Mike Knoles <MKnoles78@gmail.com>
2021-12-05 00:21:28 -0500

    added do / while loop

--- src/PerformanceAssessment1.java
+++ src/PerformanceAssessment1.java
@@ -8,16 +8,25 @@ public class PerformanceAssessment1 {
 	public static void main(String[] args) {
 		// Ask user for their age
 		Scanner input = new Scanner(System.in);
+		boolean invalid;
 
-		// takes in input from user
-		System.out.print("Please enter your age: ");
-		int age = input.nextInt();
+		do {
+			// takes in input from user
+			System.out.print("Please enter your age: ");
+			int age = input.nextInt();
 
-		if (age > 1 && age < 100) {
-			System.out.printf("Your age is: %d%n", age);
-		} else {
-			System.out.print("Your age is not within range, try again");
-		}
+			if (age > 1 && age < 100) {
+				System.out.printf("Your age is: %d%n", age);
+				invalid = false;
+			} else {
+				System.out.print("Your age is not within range. Try again.\n");
+				invalid = true;
+
+			}
+
+		} while (invalid);
+
+		System.out.println(invalid);
 
 		// if input is less than one or greater than 100 exception occurs
 		// Output message Invalid age. Please try again
