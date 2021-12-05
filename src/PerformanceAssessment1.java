1a0c32b5d071022e6ea767819b2d43852d37d93f
Mike Knoles <MKnoles78@gmail.com>
2021-12-04 22:53:13 -0500

    added scanner to take in values

--- src/PerformanceAssessment1.java
+++ src/PerformanceAssessment1.java
@@ -1,8 +1,18 @@
 
+//CIS218: Unit 4.4 Performance Assessment
+//User Entry of Age
+import java.util.Scanner;
+
 public class PerformanceAssessment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Ask user for their age
+		Scanner input = new Scanner(System.in);
+
+		// takes in input from user
+		// if input is less than one or greater than 100 exception occurs
+		// Output message Invalid age. Please try again
+		// Continues to out Invalid age, until valid input received
 
 	}
 
