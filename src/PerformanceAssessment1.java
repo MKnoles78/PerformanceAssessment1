fbd3e83baf31835f01fb53bfd77dc077f5cfc391
Mike Knoles <MKnoles78@gmail.com>
2021-12-05 00:46:31 -0500

    added try and catch

--- src/PerformanceAssessment1.java
+++ src/PerformanceAssessment1.java
@@ -1,6 +1,7 @@
 
 //CIS218: Unit 4.4 Performance Assessment
 //User Entry of Age
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PerformanceAssessment1 {
@@ -8,29 +9,30 @@ public class PerformanceAssessment1 {
 	public static void main(String[] args) {
 		// Ask user for their age
 		Scanner input = new Scanner(System.in);
-		boolean invalid;
+		boolean invalid = false;
 
 		do {
 			// takes in input from user
 			System.out.print("Please enter your age: ");
 			int age = input.nextInt();
 
-			if (age > 1 && age < 100) {
-				System.out.printf("Your age is: %d%n", age);
-				invalid = false;
-			} else {
-				System.out.print("Your age is not within range. Try again.\n");
-				invalid = true;
+			try {
+				// if input is less than one or greater than 100 exception occurs
+				if (age > 1 && age < 100) {
+					System.out.printf("%nYour age is: %d%n", age);
+					invalid = false;
+				} else {
+					invalid = true;
+					throw new InputMismatchException();
+				} // end else
+			} catch (InputMismatchException inputMismatchException) {
+				// Output message Invalid age. Please try again
+				System.out.printf("%n%s: You must enter a valid age.%n", inputMismatchException);
+				System.out.println("\nInvalid age. Please try again.\n");
 
 			}
 
-		} while (invalid);
-
-		System.out.println(invalid);
-
-		// if input is less than one or greater than 100 exception occurs
-		// Output message Invalid age. Please try again
-		// Continues to out Invalid age, until valid input received
+		} while (invalid); // Continues to output Invalid age, until valid input received
 
 	}
 
