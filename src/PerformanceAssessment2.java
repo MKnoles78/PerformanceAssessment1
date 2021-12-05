0bf9c97979d3ebe734bb8568bd715ada1f808d06
Mike Knoles <MKnoles78@gmail.com>
2021-12-05 12:56:15 -0500

    copied over PA1 to build revisions

---
+++ src/PerformanceAssessment2.java
@@ -0,0 +1,36 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PerformanceAssessment2 {
+
+	public static void main(String[] args) {
+		// Ask user for their age
+		Scanner input = new Scanner(System.in);
+		boolean invalid = false;
+
+		do {
+			// takes in input from user
+			System.out.print("Please enter your age: ");
+			int age = input.nextInt();
+
+			try {
+				// if input is less than one or greater than 100 exception occurs
+				if (age > 1 && age < 100) {
+					System.out.printf("%nYour age is: %d%n", age);
+					invalid = false;
+				} else {
+					invalid = true;
+					throw new InputMismatchException();
+				} // end else
+			} catch (InputMismatchException inputMismatchException) {
+				// Output message Invalid age. Please try again
+				System.out.printf("%n%s: You must enter a valid age.%n", inputMismatchException);
+				System.out.println("\nInvalid age. Please try again.\n");
+
+			}
+
+		} while (invalid); // Continues to output Invalid age, until valid input received
+
+	}
+
+}
